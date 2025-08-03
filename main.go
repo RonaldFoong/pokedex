9e10fc0a37411daeb3250616f2dba0fdba4c3a60
Ronald Foong <rffoong.student.unimelb.edu.au>
2025-08-03 14:08:58 +1000

    Added 2 commands

--- main.go
+++ main.go
@@ -7,16 +7,61 @@ import (
 	"os"
 )
 
+type cliCommand struct {
+	name 		string
+	description string
+	callback 	func() error
+}
+
 func main() {
+	commands := map[string]cliCommand{
+		"exit": {
+			name: "exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+		},
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: commandHelp,
+		},
+	}
 	fmt.Print("Pokedex > ")
 	scnr := bufio.NewScanner(os.Stdin)
-
+	
 	for scnr.Scan() {
 		text := cleanInput(scnr.Text())
-		fmt.Println("Your command was: " + text[0])
+		if len(text) == 0 {
+			fmt.Print("Pokedex > ")
+			continue
+		}
+
+		cmd, ok := commands[text[0]]
+		if !ok {
+			fmt.Println("Unknown command")
+		} else {
+			fmt.Println(cmd.callback())
+			if cmd.name == "exit" {
+				os.Exit(0)
+			}
+		}
+
 		fmt.Print("Pokedex > ")
 	}
 
+}
+
+func commandExit() error {
+	return fmt.Errorf("Closing the Pokedex... Goodbye!")
+}
+
+func commandHelp() error {
+	return fmt.Errorf(
+`Welcome to the Pokedex!
+Usage: 
+
+help: Displays a help message
+exit: Exit the Pokedex`)
 }
 
 func cleanInput(text string) []string {
